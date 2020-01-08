e3008afa080773557ea6fa9296afc86f0850fe6f
user <user>
2020-01-08 10:43:39 +0000

    Added an empty CommandLinerunner that will generate demo data.

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,7 +1,9 @@
 package com.packt.cardatabase;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -9,4 +11,11 @@ public class CardatabaseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner runner() {
+	return args -> {
+	    //Place your code here
+	};
+    }
 }
