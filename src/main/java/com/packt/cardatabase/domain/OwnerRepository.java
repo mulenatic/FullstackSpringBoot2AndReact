795225fe4fbaa9de32271322377b82b0e4b07df6
user <user>
2020-01-08 11:36:00 +0000

    Added a repository for the Owner.

---
+++ src/main/java/com/packt/cardatabase/domain/OwnerRepository.java
@@ -0,0 +1,11 @@
+package com.packt.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * OwnerRepository
+ */
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    
+}
