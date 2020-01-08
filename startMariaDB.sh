cef7af1c46345f1f811781ce0d94790b3a8316c6
user <user>
2020-01-08 12:28:09 +0000

    Added configuration for mariaDB connection. Seems to be running.

--- startMariaDB.sh
+++ startMariaDB.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name some-mariadb -e MYSQL_ROOT_PASSWORD=pw -e MYSQL_DATABASE=card -p 3306:3306 -d mariadb
+docker run --rm --name some-mariadb -e MYSQL_ROOT_PASSWORD=pw -e MYSQL_DATABASE=cardb -p 3306:3306 -d mariadb
