cfd583b1d9eb30db89cfaeda350637df0996ed1f
user <user>
2020-01-08 10:46:44 +0000

    Added generation of demo data.

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,5 +1,9 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +12,20 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CardatabaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardatabaseApplication.class, args);
-	}
+    @Autowired
+    private CarRepository repository;
+
+    public static void main(String[] args) {
+	SpringApplication.run(CardatabaseApplication.class, args);
+    }
 
     @Bean
     CommandLineRunner runner() {
 	return args -> {
-	    //Place your code here
+	    // Save demo data to database
+	    repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
+	    repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
+	    repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
 	};
     }
 }
