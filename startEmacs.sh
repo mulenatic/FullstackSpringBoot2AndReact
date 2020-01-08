cc7e9ce1ad6726be6f4f1d8ed43b2cd887961667
user <user>
2020-01-08 10:06:42 +0000

    Improved the emacs startup script.

--- startEmacs.sh
+++ startEmacs.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+[ ! -d lsp-conf ] && mkdir lsp-conf
+
+files=(lsp-conf/treemacs-persist lsp-conf/lsp-session-v1)
+for file in "${files[@]}"; do
+    if [ ! -f $file ];
+    then
+	echo $file is missing, touching it now
+	touch $file
+    else
+	echo $file exists
+    fi
+done
+
 docker run -it --rm \
        -v "$(pwd)":/emacsDocuments \
        -v ~/FullstackSpringBoot2AndReact/lsp-conf/lsp-session-v1:/home/docker/.emacs.d/.lsp-session-v1 \
        -v ~/FullstackSpringBoot2AndReact/lsp-conf/treemacs-persist:/home/docker/.emacs.d/.cache/treemacs-persist \
        mulenatic/emacs-docker-java
+
