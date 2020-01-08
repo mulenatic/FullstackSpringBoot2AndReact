b1473f5444f94cb34bddf8f205f6465027049926
user <user>
2020-01-08 11:53:00 +0000

    Added generation of demo owners during startup.

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -2,6 +2,8 @@ package com.packt.cardatabase;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +17,9 @@ public class CardatabaseApplication {
     @Autowired
     private CarRepository repository;
 
+    @Autowired
+    private OwnerRepository ownerRepository;
+
     public static void main(String[] args) {
 	SpringApplication.run(CardatabaseApplication.class, args);
     }
@@ -22,10 +27,16 @@ public class CardatabaseApplication {
     @Bean
     CommandLineRunner runner() {
 	return args -> {
+	    // Add owner objects and save these to db
+	    Owner owner1 = new Owner("John" , "Johnson");
+	    Owner owner2 = new Owner("Mary" , "Robinson");
+	    ownerRepository.save(owner1);
+	    ownerRepository.save(owner2);
+	    
 	    // Save demo data to database
-	    repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
-	    repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
-	    repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
+	    repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+	    repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+	    repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
 	};
     }
 }
