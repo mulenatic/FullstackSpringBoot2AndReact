7cd962445a558d721dc363b6342b0b4269e0c241
user <user>
2020-01-09 11:28:45 +0000

    Added a filter handling authenticated users.

---
+++ src/main/java/com/packt/cardatabase/AuthenticationFilter.java
@@ -0,0 +1,30 @@
+package com.packt.cardatabase;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import com.packt.cardatabase.service.AuthenticationService;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class AuthenticationFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+	    Authentication authentication = AuthenticationService.getAuthentication((HttpServletRequest) request);
+	    SecurityContextHolder.getContext().setAuthentication(authentication);
+	    chain.doFilter(request, response);
+		
+	}
+
+
+}
