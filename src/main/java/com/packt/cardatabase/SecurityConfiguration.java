a4c431d87eb21753cf0bf5b53301cca508c46e5e
user <user>
2020-01-09 10:42:52 +0000

    Removed the in memory user with dynamic users from database by using UserDetailService.

--- src/main/java/com/packt/cardatabase/SecurityConfiguration.java
+++ src/main/java/com/packt/cardatabase/SecurityConfiguration.java
@@ -1,14 +1,13 @@
 package com.packt.cardatabase;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * SecurityConfiguration
@@ -17,20 +16,21 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    
+    @Autowired
+    private UserDetailsService userDetailsService;
 
+    /*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    }*/
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+	auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+
     }
 
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-
-	UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
-
-	return new InMemoryUserDetailsManager(user);
-    }
 
     
     
