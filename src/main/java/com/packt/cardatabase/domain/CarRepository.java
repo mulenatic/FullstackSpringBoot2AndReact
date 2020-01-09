1397471d4c86c9213a9fe806cac46b8ab78ac417
user <user>
2020-01-09 07:24:25 +0000

    Added RepositoryRestResource to make custom JPA queries available.

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long>{
 
     // Fetch cars by brand
