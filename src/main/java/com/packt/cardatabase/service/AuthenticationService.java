88929eb27df01ec86475f448253be7b2ac9d4b39
user <user>
2020-01-09 11:13:28 +0000

    Added an AuthenticationService that will handle the "Authorization" header.
    This Service handles requests and responses.

---
+++ src/main/java/com/packt/cardatabase/service/AuthenticationService.java
@@ -0,0 +1,45 @@
+package com.packt.cardatabase.service;
+
+import java.util.Collections;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**
+ * AuthenticationService
+ */
+public class AuthenticationService {
+
+    static final long EXPIRATIONTIME = 864_000_00; // 1 day in milliseconds
+    static final String SIGNINGKEY = "SecretKey";
+    static final String PREFIX = "Bearer";
+
+    // Add token to Authorization header
+    static public void addToken(HttpServletResponse res, String username) {
+
+	String jwtToken = Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME)).signWith(SignatureAlgorithm.HS512, SIGNINGKEY).compact();
+	res.addHeader("Authorization", PREFIX + " " + jwtToken);
+	res.addHeader("Access-Control-Expose-Headers", "Authorization");	
+
+    }
+
+    // Get token from Authorization header
+    static public Authentication getAuthentication(HttpServletRequest request) {
+
+	String token = request.getHeader("Authorization");
+	if (token!= null) {
+	    String user = Jwts.parser().setSigningKey(SIGNINGKEY).parseClaimsJws(token.replace(PREFIX, "")).getBody().getSubject();
+	    if (user!=null) {
+		return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+	    }
+	}
+	return null;
+    }
+}
