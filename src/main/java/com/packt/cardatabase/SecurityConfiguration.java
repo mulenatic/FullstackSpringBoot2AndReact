ad02c53f2ccb28da0a29bf453bdd70f75ac12445
user <user>
2020-01-09 09:06:55 +0000

    Added a InUserManager which is good for development.

--- src/main/java/com/packt/cardatabase/SecurityConfiguration.java
+++ src/main/java/com/packt/cardatabase/SecurityConfiguration.java
@@ -1,9 +1,14 @@
 package com.packt.cardatabase;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * SecurityConfiguration
@@ -12,11 +17,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
-	}
+    
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+
+	UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+
+	return new InMemoryUserDetailsManager(user);
+    }
 
     
     
