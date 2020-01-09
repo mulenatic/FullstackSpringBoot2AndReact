4bfb190b32d7957f16c5ab51de12407260a7b147
user <user>
2020-01-09 11:39:06 +0000

    Configured Spring security to use jwt.

--- src/main/java/com/packt/cardatabase/SecurityConfiguration.java
+++ src/main/java/com/packt/cardatabase/SecurityConfiguration.java
@@ -2,12 +2,14 @@ package com.packt.cardatabase;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * SecurityConfiguration
@@ -19,10 +21,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    /*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    }*/
+
+	http.csrf().disable().cors().and().authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll()
+	    .anyRequest().authenticated()
+	    .and()
+	    // Filter for the api/login requests
+	    .addFilterBefore(new LoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+	    // Filter for other request to check JWT header
+	    .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
