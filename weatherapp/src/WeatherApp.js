3e415227cff1529fd03f1702936610e52f33d5c2
user <user>
2020-01-13 07:28:48 +0000

    Added a constructor which initializes the components state.

--- weatherapp/src/WeatherApp.js
+++ weatherapp/src/WeatherApp.js
@@ -2,6 +2,11 @@ import React, { Componet } from 'react';
 import './App.css';
 
 class WeatherApp extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {temp: 0, desc: '', icon: '', loading: true}
+    }	
+    
     render() {
 	return (
 		<div className="App">
