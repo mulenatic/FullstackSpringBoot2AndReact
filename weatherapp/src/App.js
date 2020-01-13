9dc1067a908697433a5489bb523772d6fa67f28f
user <user>
2020-01-13 07:56:29 +0000

    Added the new WeatherApp Component.

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import WeatherApp from './WeatherApp';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+	<div className="App">
+	  <WeatherApp />
+	</div>
+    );
 }
 
 export default App;
