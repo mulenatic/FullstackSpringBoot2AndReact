0f78feb9a847b8830ae0f8d2bf7932e60de28917
user <user>
2020-01-13 14:42:57 +0000

    Turned off Spring security during development.

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -21,40 +21,45 @@ import com.packt.cardatabase.service.UserDetailServiceImpl;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailServiceImpl userDetailsService; 
+    @Autowired
+    private UserDetailServiceImpl userDetailsService; 
 
-	  @Override
-	  protected void configure(HttpSecurity http) throws Exception {
-		 http.csrf().disable().cors().and().authorizeRequests()
-		  .antMatchers(HttpMethod.POST, "/login").permitAll()
-	        .anyRequest().authenticated()
-	        .and()
-	        // Filter for the api/login requests
-	        .addFilterBefore(new LoginFilter("/login", authenticationManager()),
-	                UsernamePasswordAuthenticationFilter.class)
-	        // Filter for other requests to check JWT in header
-	        .addFilterBefore(new AuthenticationFilter(),
-	                UsernamePasswordAuthenticationFilter.class);
-	  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+	// Add this row to allow access to all endpoints
+	http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
+
+	/* Commented out to turn off Security during development 
+	   http.csrf().disable().cors().and().authorizeRequests()
+	   .antMatchers(HttpMethod.POST, "/login").permitAll()
+	   .anyRequest().authenticated()
+	   .and()
+	   // Filter for the api/login requests
+	   .addFilterBefore(new LoginFilter("/login", authenticationManager()),
+	   UsernamePasswordAuthenticationFilter.class)
+	   // Filter for other requests to check JWT in header
+	   .addFilterBefore(new AuthenticationFilter(),
+	   UsernamePasswordAuthenticationFilter.class);*/
+    }
   
-	  @Bean
-	  CorsConfigurationSource corsConfigurationSource() {
-	      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	      CorsConfiguration config = new CorsConfiguration();
-			config.setAllowedOrigins(Arrays.asList("*"));
-			config.setAllowedMethods(Arrays.asList("*"));
-			config.setAllowedHeaders(Arrays.asList("*"));
-			config.setAllowCredentials(true);
-	      config.applyPermitDefaultValues();
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	CorsConfiguration config = new CorsConfiguration();
+	config.setAllowedOrigins(Arrays.asList("*"));
+	config.setAllowedMethods(Arrays.asList("*"));
+	config.setAllowedHeaders(Arrays.asList("*"));
+	config.setAllowCredentials(true);
+	config.applyPermitDefaultValues();
 	      
-	      source.registerCorsConfiguration("/**", config);
-	      return source;
-	}	
+	source.registerCorsConfiguration("/**", config);
+	return source;
+    }	
 	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-	}
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
 }
