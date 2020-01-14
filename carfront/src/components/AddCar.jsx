8296d641c722d8bc2a4c0e129faed0774be4c20d
user <user>
2020-01-14 11:41:22 +0000

    Added state to the component.

--- carfront/src/components/AddCar.jsx
+++ carfront/src/components/AddCar.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddCar = (props) => {
+
+    const [open, setOpen] = useState(false);
+    const [car, setCar] = useState({
+	brand: '', model: '', color: '', year: '', fuel: '', price: ''
+    });
+    
     return (
 	<div>
 	</div>
     );
 };
+
+export default AddCar;
