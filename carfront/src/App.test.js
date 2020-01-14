fd0236fae771b191388a25d997f970a4386bb487
user <user>
2020-01-14 14:27:33 +0000

    Added a first snapshot test.

--- carfront/src/App.test.js
+++ carfront/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import TestRenderer from 'react-test-renderer';
+import AddCar from './components/AddCar';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it ('renders a snapshot', () => {
+    const tree = TestRenderer.create(<AddCar/>).toJSON();
+    expect(tree).toMatchSnapshot();
 });
+
+
